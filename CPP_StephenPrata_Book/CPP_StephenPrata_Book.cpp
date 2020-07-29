2cf7585adcc54670d545f007badf26bf14f0b4cb
Nikolay0107 <nimphonoe@gmail.com>
2020-07-29 13:30:04 +0300

    Add project files.

---
+++ CPP_StephenPrata_Book/CPP_StephenPrata_Book.cpp
@@ -0,0 +1,30 @@
+/*
+
+Chapter 1
+Task 1
+
+*/
+
+/*
+#include <iostream>
+#include <string>
+
+using std:: string;
+using std::cout;
+using std::endl;
+
+
+
+int main()
+{
+    string adress = "My adress!";
+    int houseNumber = 555;
+    int flatNumber = 777;
+
+
+    cout << "My adress: " << adress << endl;
+    cout << "My house number: " << houseNumber << endl;
+    cout << "My flat number: " << flatNumber << endl;
+}
+
+*/
