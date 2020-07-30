b89b28fcf6147744e8ae751829576d2063f77cad
Nikolay0107 <nimphonoe@gmail.com>
2020-07-30 20:41:45 +0300

    Chapter 3 Task 1

--- CPP_StephenPrata_Book/CPP_StephenPrata_Book_Chapter_3.cpp
+++ CPP_StephenPrata_Book/CPP_StephenPrata_Book_Chapter_3.cpp
@@ -6,3 +6,32 @@ Task 1
     your height to feet and inches. Have the program use the underscore character to
     indicate where to type the response.Also use a const symbolic constant to represent the conversion factor.
 */
+
+/*
+#include <iostream>
+
+using std::string;
+using std::cout;
+using std::cin;
+using std::endl;
+
+
+int main()
+{
+	const int FOOT_TO_INCHES_RATIO = 12;
+
+	int height_in_inches{};
+	int height_in_foot{};
+	int inches_remainder{};
+
+	cout << "Please, enter your height in inches: __\b\b";
+	cin >> height_in_inches;
+	
+	height_in_foot = height_in_inches / FOOT_TO_INCHES_RATIO;
+	inches_remainder = height_in_inches % FOOT_TO_INCHES_RATIO;
+
+	cout << "\aYour height is " << height_in_foot << " foot and " << inches_remainder << " inches!\n";
+}
+*/
+
+//-----------------------------------------------------
