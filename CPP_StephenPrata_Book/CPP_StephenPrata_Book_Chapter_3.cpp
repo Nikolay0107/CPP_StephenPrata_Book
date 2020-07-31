012a3f27b15f764e05a963dcb8ed0b7f164c9216
Nikolay0107 <nimphonoe@gmail.com>
2020-07-31 16:59:32 +0300

    Chapter 3 Task 5

--- CPP_StephenPrata_Book/CPP_StephenPrata_Book_Chapter_3.cpp
+++ CPP_StephenPrata_Book/CPP_StephenPrata_Book_Chapter_3.cpp
@@ -189,3 +189,44 @@ int main()
 	cout << number_of_seconds << " seconds = " << days << " days, " << hours << " hours, " << minutes << " minutes, " << seconds << " seconds" << endl;
 }
 */
+
+/*
+Chapter 3
+Task 5
+
+	Write a program that requests the user to enter the current world population and
+	the current population of the U.S. (or of some other nation of your choice). Store
+	the information in variables of type long long. Have the program display the percent that the U.S.
+	(or other nation’s) population is of the world’s population.The output should look something like this:
+	
+		Enter the world's population: 6898758899
+		Enter the population of the US: 310783781
+		The population of the US is 4.50492% of the world population.
+		You can use the Internet to get more recent figures
+*/
+
+
+#include <iostream>
+
+using std::string;
+using std::cout;
+using std::cin;
+using std::endl;
+
+int main()
+{
+
+
+	cout << "Enter the world's population: ";
+	int world_population{};
+	cin >> world_population;
+	cout << "\nEnter the population of your country: ";
+	int country_population{};
+	cin >> country_population;
+
+	int percent_of_population{};
+
+	percent_of_population = country_population/world_population;
+	
+	cout << "The population of your country is " << percent_of_population << "% of the world population." << endl;
+}
