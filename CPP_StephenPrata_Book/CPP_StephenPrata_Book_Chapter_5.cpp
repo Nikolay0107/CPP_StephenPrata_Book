95bd9fcd7eae77c4834917f6d1451e9bc4dde385
Nikolay0107 <nimphonoe@gmail.com>
2020-08-09 12:05:36 +0300

    Chapter 5 Task 7

--- CPP_StephenPrata_Book/CPP_StephenPrata_Book_Chapter_5.cpp
+++ CPP_StephenPrata_Book/CPP_StephenPrata_Book_Chapter_5.cpp
@@ -224,7 +224,7 @@ combined years.
 
 */
 
-
+/*
 #include <iostream>
 #include <string>
 
@@ -257,15 +257,15 @@ int main()
 
 		for (int month = 0; month < ALL_MONTH; month++)
 		{
-			cout << "Please, enter the number of the books, which was sold in " << months_of_the_year[month] << " in year " << year+1<< ": ";
+			cout << "Please, enter the number of the books, which were sold in " << months_of_the_year[month] << " in the year " << year+1<< ": ";
 			cin >> arr[year][month];
 			yearlySales[year] += arr[year][month];
 		}
-		cout << "In the year <" << year + 1 << "> was sold " << yearlySales[year] << " books!" << endl;
+		cout << "In the year <" << year + 1 << "> were sold " << yearlySales[year] << " books!" << endl;
 		*total_sales += yearlySales[year];
 	}
 
-	cout << "There are " << *total_sales << " books was sold in total!" << endl;
+	cout << "It were sold " << *total_sales << " books in total!" << endl;
 
 	///////////////////// Memory clear
 
@@ -278,3 +278,75 @@ int main()
 	delete total_sales;
 	return 0 ;
 }
+*/
+
+
+/*
+Chapter 5
+Task 7
+
+Design a structure called car that holds the following information about an automobile: its make,as a string in a character
+array or in a string object,and the year it was built,as an integer.Write a program that asks the user how many cars to catalog.
+The program should then use new to create a dynamic array of that many car structures. Next, it should prompt the user to input
+the make (which might consist of more than one word) and year information for each structure. Note that this
+requires some care because it alternates reading strings with numeric data (see Chapter 4). Finally, it should display the contents
+of each structure.A sample run should look something like the following:
+	
+	How many cars do you wish to catalog? 2
+	Car #1:
+	Please enter the make: Hudson Hornet
+	Please enter the year made: 1952
+	Car #2:
+	Please enter the make: Kaiser
+	Please enter the year made: 1951
+	Here is your collection:
+	1952 Hudson Hornet
+	1951 Kaiser
+*/
+/*
+#include <iostream>
+#include <string>
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::string;
+
+struct car_str
+{
+	string car_make;
+	int car_build_year;
+};
+
+int main()
+{
+	cout << "How many cars do you wish to catalog? ";
+		unsigned int cars_in_catalog{};
+		cin >> cars_in_catalog;
+
+		car_str* car_arr = new car_str[cars_in_catalog];
+
+		for (int i = 0; i < cars_in_catalog; i++)
+		{
+			cin.get(); // ?????
+			
+			cout << "Car #" << (i + 1) << ": " << endl;
+			cout << "\tPlease enter the make: ";
+			getline(cin, car_arr[i].car_make);
+
+			cout <<endl << "\tPlease enter the year made: ";
+			cin >> car_arr[i].car_build_year;
+		}
+
+		cout << "\nHere is your collection:" << endl;
+		
+		for (int i = 0; i < cars_in_catalog; i++)
+		{
+			cout << "\t"<< car_arr[i].car_make << "\t" << car_arr[i].car_build_year << endl;
+		}
+			
+		delete[] car_arr;
+
+	return 0;
+}
+*/
