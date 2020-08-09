19c3a76bdb968074f0c8053d8a7e52fea331ea44
Nikolay0107 <nimphonoe@gmail.com>
2020-08-09 13:15:25 +0300

    Chapter 5 Task 8

--- CPP_StephenPrata_Book/CPP_StephenPrata_Book_Chapter_5.cpp
+++ CPP_StephenPrata_Book/CPP_StephenPrata_Book_Chapter_5.cpp
@@ -328,7 +328,7 @@ int main()
 
 		for (int i = 0; i < cars_in_catalog; i++)
 		{
-			cin.get(); // ?????
+			cin.get(); // clear the symbol of new line
 			
 			cout << "Car #" << (i + 1) << ": " << endl;
 			cout << "\tPlease enter the make: ";
@@ -346,6 +346,51 @@ int main()
 		}
 			
 		delete[] car_arr;
+
+	return 0;
+}
+*/
+
+/*
+Chapter 5
+Task 8
+
+Write a program that uses an array of char and a loop to read one word at a time
+until the word done is entered.The program should then report the number of
+words entered (not counting done).A sample run could look like this:
+Enter words (to stop, type the word done):
+	anteater birthday category dumpster
+	envy finagle geometry done for sure
+	You entered a total of 7 words.
+You should include the cstring header file and use the strcmp() function to
+make the comparison test.
+*/
+
+/*
+#include <iostream>
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+const int STRING_LENGTH = 30;
+
+int main()
+{
+	cout << "Enter words (to stop, enter the word 'done'):" << endl;
+
+	char inputWord[STRING_LENGTH];
+	cin >> inputWord;
+
+	unsigned short counter{};
+
+	while (strcmp(inputWord, "done"))
+	{
+		counter++;
+		cin >> inputWord;
+	}
+
+	cout << "You entered a total of " << counter << " words." << endl;
 
 	return 0;
 }
