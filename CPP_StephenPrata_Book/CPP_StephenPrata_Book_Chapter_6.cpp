18df31abaa6b977fb6b8d4287c8803cafb69b6c6
Nikolay0107 <nimphonoe@gmail.com>
2020-08-12 19:31:49 +0300

    Chapter 6 Task 7

--- CPP_StephenPrata_Book/CPP_StephenPrata_Book_Chapter_6.cpp
+++ CPP_StephenPrata_Book/CPP_StephenPrata_Book_Chapter_6.cpp
@@ -449,7 +449,7 @@ Task 6
 	two categories, the program need do no sorting.
 
 */
-
+/*
 #include <iostream>
 #include <string>
 
@@ -528,4 +528,22 @@ int main()
 
 	return 0;
 }
+*/
+
+/*
+Chapter 6
+Task 7
+	Write a program that reads input a word at a time until a lone q is entered.The
+	program should then report the number of words that began with vowels, the number that began with consonants,and the number that fit neither of those categories.
+	One approach is to use isalpha() to discriminate between words beginning with
+	letters and those that don’t and then use an if or switch statement to further identify those passing the isalpha() test that begin with vowels.A sample run might
+	look like this:
+						Enter words (q to quit):
+						The 12 awesome oxen ambled
+						quietly across 15 meters of lawn. q
+						5 words beginning with vowels
+						4 words beginning with consonants
+						2 others
+*/
+
 
