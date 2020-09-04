29428f9ad9beba63c27eafc69abb38859f0ad505
Nikolay0107 <nimphonoe@gmail.com>
2020-09-04 13:55:46 +0300

    Chapter 7 Task 5

--- CPP_StephenPrata_Book/CPP_StephenPrata_Book_Chapter_7.cpp
+++ CPP_StephenPrata_Book/CPP_StephenPrata_Book_Chapter_7.cpp
@@ -392,3 +392,54 @@ long double probability(unsigned numbers, unsigned picks, unsigned meganumber_ra
 }
 
 */
+
+
+/*
+Chapter 7
+Task 5
+
+Define a recursive function that takes an integer argument and returns the factorial
+of that argument. Recall that 3 factorial, written 3!, equals 3 × 2!,and so on, with 0!
+defined as 1. In general, if n is greater than zero, n! = n * (n - 1)!.Test your function
+in a program that uses a loop to allow the user to enter various values for which the
+program reports the factorial.
+
+*/
+
+/*
+#include <iostream>
+using namespace std;
+
+unsigned long long factorial_function( int number);
+
+int main()
+{
+	int number;
+
+	cout << "Enter the number: \n";
+
+	while (cin >> number)
+	{
+		cin.get();
+		while (number < 0)
+		{
+			cout << "\n\aInvalid input. Please enter a positive integer: ";
+			cin >> number;
+		}
+
+		cout << endl << endl << "Result: "  << factorial_function(number) << endl << endl;
+		cout << "Enter the number: \n";
+	}
+
+	return 0;
+}
+
+unsigned long long factorial_function(int number)
+{
+	if (number <= 1)
+		return 1;
+	else
+		return number * (factorial_function(number - 1));
+}
+
+*/
